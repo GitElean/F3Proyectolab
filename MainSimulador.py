e1005f59a9ae37f2c7735383831510eef4a1193a
GitElean <47782508+GitElean@users.noreply.github.com>
2021-09-29 14:37:57 -0600

    Create MainSimulador.py

---
+++ MainSimulador.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 29 12:45:23 2021
+
+@author: Usuario
+"""
+import tkinter as ttk
+
+window = ttk()
+window.title("Simulador de tubos catódicos")
+
+window.mainloop()
