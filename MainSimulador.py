eb859c510bc1661501928723a0f835f97e062ee8
Mirka <mirkanicolle@gmail.com>
2021-09-29 15:20:29 -0600

    prop

--- MainSimulador.py
+++ MainSimulador.py
@@ -2,11 +2,21 @@
 """
 Created on Wed Sep 29 12:45:23 2021
 
-@author: Usuario
+@author: Elean Rivas 
+         Mirka Monzon 18139
+         Cristopher Barrios 18207
+         El Elean
 """
-import tkinter as ttk
 
-window = ttk()
+import tkinter as tk
+import matplotlib
+
+window = tk.Tk()
 window.title("Simulador de tubos catódicos")
 
+
+
 window.mainloop()
+#Ecuaciones 
+#1
+#2
