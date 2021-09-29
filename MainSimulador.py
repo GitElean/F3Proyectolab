4d65d8cae26bff1b14434d7f9444b8bacc3c7f10
Mirka <mirkanicolle@gmail.com>
2021-09-29 15:34:17 -0600

    labels

--- MainSimulador.py
+++ MainSimulador.py
@@ -9,12 +9,18 @@ Created on Wed Sep 29 12:45:23 2021
 """
 
 import tkinter as tk
+from tkinter import font
 import matplotlib
 
 window = tk.Tk()
 window.title("Simulador de tubos catódicos")
+window.config(background="black")
 
-
+"""input"""
+tk.Label(window, text="Amplitud", bg="black", fg="white", font="none 12 bold") .grid(row=1, column=0)
+tk.Label(window, text="Frecuencia", bg="black", fg="white", font="none 12 bold") .grid(row=1, column=0)
+tk.Label(window, text="Amplitud", bg="black", fg="white", font="none 12 bold") .grid(row=1, column=0)
+tk.Label(window, text="Amplitud", bg="black", fg="white", font="none 12 bold") .grid(row=1, column=0)
 
 window.mainloop()
 #Ecuaciones 
